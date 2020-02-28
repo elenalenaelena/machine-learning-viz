bad15d20894dcb48c417750073963f42b37bd26a
Elena Stoll <elenalenastoll@gmail.com>
2020-02-28 15:32:20 +0100

    d3 integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
   
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 
@@ -14,7 +13,7 @@ import { BarChartComponent } from './bar-chart/bar-chart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
